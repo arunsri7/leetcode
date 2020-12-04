5b08f0ee461a5c414cb8eaba2f683ebfaa8499a0
“arunsri7” <arun.i1997@gmail.com>
2020-12-05 01:25:03 +0530

    [binary-search] binary approach for sqrt.js

--- binary-search/sqrt.js
+++ binary-search/sqrt.js
@@ -1,7 +1,15 @@
-/**
- * @param {number} x
- * @return {number}
- */
-var mySqrt = function(x) {
-    return Math.floor(Math.sqrt(x))
-};
+let sqrt =(number, low, high) => {
+  let guess = (low + high) / 2;
+  let approx = guess * guess;
+  if (Math.abs(number - approx) < 0.01) return Math.floor(guess);
+  if (approx > number)
+    high = guess;
+  else
+    low = guess;
+  return sqrt(number, low, high);
+}
+
+let mySqrt = (number) => {
+  if (number < 2) return number;
+  return sqrt(number, 0, number);
+}

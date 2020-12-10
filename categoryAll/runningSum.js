652cee77c7e6e299e9265ca9313f536cfe260bee
“arunsri7” <arun.i1997@gmail.com>
2020-12-10 12:16:18 +0530

    [1480] Running Sum

---
+++ categoryAll/runningSum.js
@@ -0,0 +1,11 @@
+var runningSum = function(nums) {
+    arr = Array()
+    for(i=1;i<nums.length+1;i++){
+        a = nums.slice(0,i)
+        a = a.reduce(sum)
+        arr.push(a)
+    }
+    return arr
+};
+
+let sum = (a,b) => a+ba
